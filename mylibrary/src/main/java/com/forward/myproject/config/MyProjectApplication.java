d75c083e1186c028f5be5486e09140f44e86413f
huangziwei <huangziwei1@bigo.sg>
2018-09-01 18:31:44 +0800

    1.0.3

--- mylibrary/src/main/java/com/forward/myproject/config/MyProjectApplication.java
+++ mylibrary/src/main/java/com/forward/myproject/config/MyProjectApplication.java
@@ -13,9 +13,8 @@ public class MyProjectApplication extends Application {
     public void onCreate() {
         super.onCreate();
         CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(this);
-        strategy.setAppPackageName(getPackageName());
         strategy.setAppChannel("" + getApplicationInfo().loadLabel(getPackageManager()));
-        CrashReport.initCrashReport(getApplicationContext(), APP_ID, true);
+        CrashReport.initCrashReport(getApplicationContext(), APP_ID, true, strategy);
         Toast.makeText(this, "MyProjectApplication", Toast.LENGTH_LONG).show();
     }
 }
